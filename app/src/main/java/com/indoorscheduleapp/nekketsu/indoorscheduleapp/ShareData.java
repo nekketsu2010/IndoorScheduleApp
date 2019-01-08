8afae2e41a3431214c584603922b18013e45e24c
nekketsu2010 <41464917+nekketsu2010@users.noreply.github.com>
2019-01-08 20:48:25 +0900

    0108LabWindows

--- app/src/main/java/com/indoorscheduleapp/nekketsu/indoorscheduleapp/ShareData.java
+++ app/src/main/java/com/indoorscheduleapp/nekketsu/indoorscheduleapp/ShareData.java
@@ -5,5 +5,5 @@ import java.util.ArrayList;
 public class ShareData {
     public static ArrayList<RoomClass> roomClasses = new ArrayList<>();
 
-    public static String url = "http://d0259c06.ngrok.io//sample-game-server/";
+    public static String url = "http://6a0c32d8.ngrok.io//sample-game-server/";
 }
